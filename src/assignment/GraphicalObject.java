23ad64167c3562dbf2856f162adfb37eb0deb302
BVenom1 <bvenom169@gmail.com>
2022-06-28 18:40:31 -0400

    first implementation of Vertex

--- src/assignment/GraphicalObject.java
+++ src/assignment/GraphicalObject.java
@@ -3,21 +3,21 @@ package assignment;
 public abstract class GraphicalObject {
 	public abstract void transform(double[][] transformMatrix);
 	
-	public void rotateXAxis(double angle) {
+	public void rotateXAxis(double angle) {		// use transform with rotation matrix around x axis
 		double [][] rotationMatrix = {	{	1,		0,					0					},
 										{	0, 		Math.cos(angle), 	-Math.sin(angle)	}, 
 										{	0,		Math.sin(angle),	Math.cos(angle)		}};
 		transform(rotationMatrix);
 	}
 	
-	public void rotateYAxis(double angle) {
+	public void rotateYAxis(double angle) {		// use transform with rotation matrix around y axis
 		double [][] rotationMatrix = {	{	Math.cos(angle),	0,	Math.sin(angle)	},
 										{	0, 					1, 	0				}, 
 										{	-Math.sin(angle),	0,	Math.cos(angle)	}};
 		transform(rotationMatrix);
 	}
 	
-	public void rotateZAxis(double angle) {
+	public void rotateZAxis(double angle) {		// use transform with rotation matrix around z axis
 		double [][] rotationMatrix = {	{	Math.cos(angle),	-Math.sin(angle),	0	},
 										{	Math.sin(angle), 	Math.cos(angle), 	0	}, 
 										{	0,					0,					1	}};
---
+++ src/assignment/Vertex.java
@@ -0,0 +1,54 @@
+package assignment;
+
+import java.util.Objects;
+
+public class Vertex extends GraphicalObject{
+	double x;
+	double y;
+	double z;
+	public Vertex(double x, double y, double z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	@Override
+	public void transform(double[][] transformMatrix) {			// matrix vector multiplication
+		double newX = 	transformMatrix[0][0] * x +
+						transformMatrix[0][1] * y +
+						transformMatrix[0][2] * z	;
+		
+		double newY = 	transformMatrix[1][0] * x +
+						transformMatrix[1][1] * y +
+						transformMatrix[1][2] * z	;
+		
+		double newZ = 	transformMatrix[2][0] * x +
+						transformMatrix[2][1] * y +
+						transformMatrix[2][2] * z	;
+		
+		x = newX;
+		y = newY;
+		z = newZ;
+				
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y, z);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || !(obj instanceof Vertex))
+			return false;
+		Vertex other = (Vertex) obj;
+		return (x == other.x && y == other.y && z == other.z);
+	}
+	@Override
+	public String toString() {
+		return x + " " + y + " " + z;
+	}
+	
+	
+	
+}
