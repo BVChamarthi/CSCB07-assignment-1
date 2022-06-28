e05ffb95507ee91b70b57664424ff36a1e93ed71
BVenom1 <bvenom169@gmail.com>
2022-06-28 18:52:27 -0400

    first implementation of Polygon

---
+++ src/assignment/Polygon.java
@@ -0,0 +1,37 @@
+package assignment;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+
+public class Polygon extends GraphicalObject{
+
+	LinkedHashSet<Vertex> vertices;
+
+	public Polygon(LinkedHashSet<Vertex> vertices) {
+		super();
+		this.vertices = vertices;
+	}
+
+	@Override
+	public void transform(double[][] transformMatrix) {
+		for(Vertex vertex : vertices)
+			vertex.transform(transformMatrix);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(vertices);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || !(obj instanceof Polygon))
+			return false;
+		Polygon other = (Polygon) obj;
+		return Objects.equals(vertices, other.vertices);
+	}
+
+	
+}
